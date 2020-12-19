2a42317f49f46ed61bda7cd1bed5457cd3072173
brenu <bvsousa64@gmail.com>
2020-12-19 20:20:15 -0300

    :rocket: Make workarounds in the forum engine

--- backend/api/teacher/forumAnswer.js
+++ backend/api/teacher/forumAnswer.js
@@ -1,65 +1,65 @@
-const knex = require('../../config/db')
+const knex = require("../../config/db");
 
-module.exports = app => {
-    const { existsOrError } = app.api.validator
+module.exports = (app) => {
+  const { existsOrError } = app.api.validator;
 
-    const get = async (req, res) => {
-        try {
-            existsOrError(req.params.id, 'forumAnswer does not exist!')
-    
-            const getIdForumAnswer = await knex('forumAnswer')
-                .where({ forumTopic_id: req.params.id }).first()
-            existsOrError(getIdForumAnswer, 'forumAnswer not found')
+  const get = async (req, res) => {
+    try {
+      existsOrError(req.params.id, "forumAnswer does not exist!");
 
-            res.json(getIdForumAnswer)
-        } catch (msg) {
-            return res.status(400).send(msg)
-        }
+      const getIdForumAnswer = await knex("forumAnswer")
+        .where({ forumTopic_id: req.params.id })
+        .select("*");
+
+      res.json(getIdForumAnswer);
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
+  };
 
-    const remove = async (req, res) => {
-        try {
-            existsOrError(req.params.id, 'forumAnswer does not exist!')
+  const remove = async (req, res) => {
+    try {
+      existsOrError(req.params.id, "forumAnswer does not exist!");
 
-            const removeForumAnswer = await app.db('forumAnswer').del()
-                .where({ forumAnswer_id: req.params.id })
-            existsOrError(removeForumAnswer, 'forumAnswer not found')
+      const removeForumAnswer = await app
+        .db("forumAnswer")
+        .del()
+        .where({ forumAnswer_id: req.params.id });
+      existsOrError(removeForumAnswer, "forumAnswer not found");
 
-            res.status(204).send()
-        }
-        catch (msg) {
-            return res.status(400).send(msg)
-        }
+      res.status(204).send();
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
+  };
 
-    const post = async (req, res) => {
-        const forumAnswer = req.body;
-        try {
-            const newForumAnswer = await knex("forumAnswer").insert(forumAnswer)
-            res.json(newForumAnswer);
-        }catch (err) {
-            console.log(res);
-            return res.status(500).send(err);
-        }
+  const post = async (req, res) => {
+    const forumAnswer = req.body;
+    try {
+      const newForumAnswer = await knex("forumAnswer").insert(forumAnswer);
+      res.json(newForumAnswer);
+    } catch (err) {
+      console.log(res);
+      return res.status(500).send(err);
     }
+  };
 
-    const put = async (req, res) => {
-        const forumAnswer = req.body;
-        const forumAnswer_id = req.params.id;
-        try{
-            existsOrError(forumAnswer_id, 'forumAnswer does not exist!')
-            
-            const attForumAnswer = await knex("forumAnswer")
-                .update(forumAnswer)
-                .where({ forumAnswer_id: forumAnswer_id })
-            existsOrError(attForumAnswer, 'forumAnswer not found')
-            
-            res.status(200).send();
-        } catch(msg) {
-            return res.status(400).send(msg);   
-        }
+  const put = async (req, res) => {
+    const forumAnswer = req.body;
+    const forumAnswer_id = req.params.id;
+    try {
+      existsOrError(forumAnswer_id, "forumAnswer does not exist!");
+
+      const attForumAnswer = await knex("forumAnswer")
+        .update(forumAnswer)
+        .where({ forumAnswer_id: forumAnswer_id });
+      existsOrError(attForumAnswer, "forumAnswer not found");
+
+      res.status(200).send();
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
-    
+  };
 
-    return { get, post, put, remove }
-}
+  return { get, post, put, remove };
+};
--- backend/api/teacher/forumTopic.js
+++ backend/api/teacher/forumTopic.js
@@ -1,65 +1,65 @@
-const knex = require('../../config/db')
+const knex = require("../../config/db");
 
-module.exports = app => {
-    const { existsOrError } = app.api.validator
+module.exports = (app) => {
+  const { existsOrError } = app.api.validator;
 
-    const get = async (req, res) => {
-        try {
-            existsOrError(req.params.id, 'forumTopic does not exist!')
-    
-            const getIdForumTopic = await knex('forumTopic')
-                .where({ subject_id: req.params.id }).first()
-            existsOrError(getIdForumTopic, 'forumTopic not found')
+  const get = async (req, res) => {
+    try {
+      existsOrError(req.params.id, "forumTopic does not exist!");
 
-            res.json(getIdForumTopic)
-        } catch (msg) {
-            return res.status(400).send(msg)
-        }
+      const getIdForumTopic = await knex("forumTopic")
+        .where({ subject_id: req.params.id })
+        .select("*");
+
+      res.json(getIdForumTopic);
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
+  };
 
-    const remove = async (req, res) => {
-        try {
-            existsOrError(req.params.id, 'forumTopic does not exist!')
+  const remove = async (req, res) => {
+    try {
+      existsOrError(req.params.id, "forumTopic does not exist!");
 
-            const removeForumTopic = await app.db('forumTopic').del()
-                .where({ forumTopic_id: req.params.id })
-            existsOrError(removeForumTopic, 'forumTopic not found')
+      const removeForumTopic = await app
+        .db("forumTopic")
+        .del()
+        .where({ forumTopic_id: req.params.id });
+      existsOrError(removeForumTopic, "forumTopic not found");
 
-            res.status(204).send()
-        }
-        catch (msg) {
-            return res.status(400).send(msg)
-        }
+      res.status(204).send();
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
+  };
 
-    const post = async (req, res) => {
-        const forumTopic = req.body;
-        try {
-            const newForumTopic = await knex("forumTopic").insert(forumTopic)
-            res.json(newForumTopic);
-        }catch (err) {
-            console.log(res);
-            return res.status(500).send(err);
-        }
+  const post = async (req, res) => {
+    const forumTopic = req.body;
+    try {
+      const newForumTopic = await knex("forumTopic").insert(forumTopic);
+      res.json(newForumTopic);
+    } catch (err) {
+      console.log(res);
+      return res.status(500).send(err);
     }
+  };
 
-    const put = async (req, res) => {
-        const forumTopic = req.body;
-        const forumTopic_id = req.params.id;
-        try{
-            existsOrError(forumTopic_id, 'forumTopic does not exist!')
-            
-            const attForumTopic = await knex("forumTopic")
-                .update(forumTopic)
-                .where({ forumTopic_id: forumTopic_id })
-            existsOrError(attForumTopic, 'forumTopic not found')
-            
-            res.status(200).send();
-        } catch(msg) {
-            return res.status(400).send(msg);   
-        }
+  const put = async (req, res) => {
+    const forumTopic = req.body;
+    const forumTopic_id = req.params.id;
+    try {
+      existsOrError(forumTopic_id, "forumTopic does not exist!");
+
+      const attForumTopic = await knex("forumTopic")
+        .update(forumTopic)
+        .where({ forumTopic_id: forumTopic_id });
+      existsOrError(attForumTopic, "forumTopic not found");
+
+      res.status(200).send();
+    } catch (msg) {
+      return res.status(400).send(msg);
     }
-    
+  };
 
-    return { get, post, put, remove }
-}
+  return { get, post, put, remove };
+};
